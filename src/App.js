d88f557406a3564839ac5b9613d5435d0f3b50b2
Huseyın Bura <hsyn.bura@gmail.com>
2023-04-11 20:40:25 +0300

    improve react-practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -20,6 +16,7 @@ function App() {
           <Route path="/" exact Component={Home}></Route>
           <Route path="/menu" exact Component={Menu}></Route>
           <Route path="/about" exact Component={About}></Route>
+          <Route path="/contact" exact Component={Contact}></Route>
         </Routes>
         <Footer />
       </Router>
